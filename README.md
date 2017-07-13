f8f4a7054d35da7ca32fef90b1a79b88beae4afe
Iuri Lugão Machado <imetallica@Mac-mini-de-Iuri.local>
2017-07-13 00:48:10 -0300

    Added dependencies to Python Project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lugik
+
+## Using Docker 
+To build/run: `docker-compose up --build`
+To delete all: `docker system prune -af`
--- lugik/app.py
+++ lugik/app.py
@@ -1,2 +1,22 @@
 import eventlet
 
+# Monkeypatch all the things :S
+eventlet.monkey_patch()
+
+
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+sio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+  """
+  Just return 'It works!'
+  """
+  return "It works!"
+
+if __name__ == '__main__':
+  sio.run(app, host="0.0.0.0", port=5000, debug=True)
